2f9a99db7123fc85af469466331471350b608462
Siesta <20047950+deepsiesta@users.noreply.github.com>
2024-09-18 21:54:44 +0200

    Fix audio delay

--- configuration.nix
+++ configuration.nix
@@ -82,15 +82,23 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    wireplumber.extraConfig = {
+      disable-session-timeout = {
+        "monitor.alsa.rules" = [{
+          matches = [
+            { "node.name" = "~alsa_input.*"; }
+            { "node.name" = "~alsa_output.*"; }
+          ];
+          actions.update-props."session.suspend-timeout-seconds" = 0;
+        }];
+      };
+    };
   };
 
   ## Nvidia stuff
