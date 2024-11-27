ff541871bb24094872546cd48910cf304e62d351
Siesta <20047950+deepsiesta@users.noreply.github.com>
2024-11-27 18:31:04 +0100

    Bluetooth controls

--- hosts/satella/configuration.nix
+++ hosts/satella/configuration.nix
@@ -36,6 +36,11 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
   # Set your time zone.
   time.timeZone = "Europe/Madrid";
 
