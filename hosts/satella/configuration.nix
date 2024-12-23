e279ba5b8ca7626a407b82df7542e9ea6007aad5
Siesta <20047950+deepsiesta@users.noreply.github.com>
2024-12-23 03:03:33 +0100

    Add gstreamer

--- hosts/satella/configuration.nix
+++ hosts/satella/configuration.nix
@@ -232,6 +232,13 @@
     dive # Look inside images
     podman-tui
     podman-compose
+    # Gstreamer
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav
   ];
 
   # Fonts
