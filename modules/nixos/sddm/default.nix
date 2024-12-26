d79264ce2c74326ab0e93c0b01c0c53e839b5a96
Siesta <20047950+deepsiesta@users.noreply.github.com>
2024-12-26 20:11:58 +0100

    SDDM theming

--- modules/nixos/sddm/default.nix
+++ modules/nixos/sddm/default.nix
@@ -1,9 +1,23 @@
-{...}: {
+{pkgs, ...}: let
+  sddm-astronaut = pkgs.sddm-astronaut.override {
+    themeConfig = {
+      Font = "Noto Sans";
+      AccentColor = "#FFFFFF";
+      PartialBlur = false;
+      FormPosition = "center";
+      ForceHideCompletePassword = true;
+    };
+  };
+in {
   services.displayManager = {
     defaultSession = "hyprland-uwsm";
     sddm = {
       enable = true;
       wayland.enable = true;
+      package = pkgs.kdePackages.sddm; # Use Qt6
+      theme = "sddm-astronaut-theme";
+      extraPackages = [sddm-astronaut];
     };
   };
+  environment.systemPackages = [sddm-astronaut];
 }
