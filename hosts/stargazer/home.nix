601ae08181107e0f8f53605a08fb5d4d9c0a67e7
Siesta <20047950+deepsiesta@users.noreply.github.com>
2025-01-18 02:34:22 +0100

    Fix monitor issues, hopefully

--- hosts/stargazer/home.nix
+++ hosts/stargazer/home.nix
@@ -37,8 +37,8 @@
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [
-        "DP-3, 2560x1440@144, 0x0, 1"
-        "DP-1, 2560x1440@144, 2560x0, 1"
+        "DP-3, 2560x1440@120, 0x0, 1"
+        "DP-1, 2560x1440@120, 2560x0, 1"
       ];
       workspace =
         [
