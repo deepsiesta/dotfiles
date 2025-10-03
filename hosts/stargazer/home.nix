64673e0d040981573ac746d9c8797d30ca3bdd35
Siesta <20047950+deepsiesta@users.noreply.github.com>
2025-10-03 21:30:56 +0200

    Fix offset typo

--- hosts/stargazer/home.nix
+++ hosts/stargazer/home.nix
@@ -37,7 +37,7 @@
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [
-        "HDMI-A-1, 3840x2160@60, -1920, 2, bitdepth, 10"
+        "HDMI-A-1, 3840x2160@60, -1920x0, 2, bitdepth, 10"
         "DP-1, 2560x1440@144, 0x0, 1, bitdepth, 10"
       ];
       workspace =
