49e13c56470b93f92df120c0f8b91b64b8eaebd5
Siesta <20047950+deepsiesta@users.noreply.github.com>
2026-01-04 15:21:31 +0100

    Update

--- modules/nixos/gui/default.nix
+++ modules/nixos/gui/default.nix
@@ -8,7 +8,7 @@
 
   # Thunar
   programs.thunar.enable = true;
-  programs.thunar.plugins = with pkgs.xfce; [thunar-archive-plugin thunar-volman];
+  programs.thunar.plugins = with pkgs; [thunar-archive-plugin thunar-volman];
   services.gvfs.enable = true; # Mount, trash, and other functionalities
   services.tumbler.enable = true; # Thumbnail support for images
 
