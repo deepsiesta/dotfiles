0039ae2d8890e212b080f4d7ee70deb2adacd07d
Siesta <20047950+deepsiesta@users.noreply.github.com>
2026-02-01 18:44:28 +0100

    Add CUDA support to OBS

--- modules/nixos/obs/default.nix
+++ modules/nixos/obs/default.nix
@@ -1,6 +1,11 @@
 {pkgs, ...}: {
   programs.obs-studio = {
     enable = true;
+    package = (
+      pkgs.obs-studio.override {
+        cudaSupport = true;
+      }
+    );
     plugins = with pkgs.obs-studio-plugins; [
       obs-vkcapture
     ];
