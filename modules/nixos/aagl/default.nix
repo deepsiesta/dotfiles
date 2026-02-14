99149fcfb6c969245829ccdf46e8a63cbacd0bd0
Siesta <20047950+deepsiesta@users.noreply.github.com>
2026-02-14 17:15:13 +0100

    Add missing winetricks dependency

--- modules/nixos/aagl/default.nix
+++ modules/nixos/aagl/default.nix
@@ -3,5 +3,6 @@
   programs = {
     anime-game-launcher.enable = true;
     honkers-railway-launcher.enable = true;
+    honkers-railway-launcher.package = inputs.aagl.packages.x86_64-linux.honkers-railway-launcher.override {extraPkgs = pkgs: [pkgs.winetricks];};
   };
 }
