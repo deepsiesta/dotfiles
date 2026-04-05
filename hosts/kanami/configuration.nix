50159a6ebc26808c33c45828f842c4de74a81d49
Siesta <20047950+deepsiesta@users.noreply.github.com>
2026-04-05 18:48:52 +0200

    Add wifi suport for old desktop

--- hosts/kanami/configuration.nix
+++ hosts/kanami/configuration.nix
@@ -1,5 +1,6 @@
 {
   flake.modules.nixos.kanami = {
+    config,
     pkgs,
     inputs,
     lib,
@@ -20,7 +21,7 @@
       "hyprland"
       "stylix"
       "gaming"
-      "nvidia"
+      # "nvidia"
       "neovim"
       "starship"
       "tmux"
@@ -34,7 +35,7 @@
     ];
 
     # Kernel
-    boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+    boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_12; # Last LTS version with working wifi driver
     hardware.cpu.intel.updateMicrocode = true;
 
     # Bootloader.
@@ -51,6 +52,20 @@
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+    # Required by wireless card
+    hardware.usb-modeswitch.enable = true;
+    boot.initrd.kernelModules = ["8852au"];
+    boot.extraModulePackages = [
+      config.boot.kernelPackages.rtl8852au
+      config.boot.kernelPackages.rtl8852bu
+    ];
+    # environment.systemPackages = [pkgs.linuxKernel.packages.linux_5_15.rtw89];
+    services.udev.extraRules =
+      # Switch card from storage mode to WiFi mode.
+      ''
+        ATTR{idVendor}=="0bda", ATTR{idProduct}=="1a2b", RUN+="${lib.getExe pkgs.usb-modeswitch} -K -v 0bda -p 1a2b"
+      '';
+
     # Enable OpenGL
     hardware.graphics.enable = true;
 
@@ -58,7 +73,7 @@
     services.flatpak.enable = true;
 
     # Old GPUs cannot use the open kernel module
-    host.nvidia.legacy = true;
+    # host.nvidia.legacy = true;
 
     home-manager.users.siesta = {
       host.monitors = [
