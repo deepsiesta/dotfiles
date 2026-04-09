f8a138b0a3e810184823363ca92d338a07a7faf7
Siesta <20047950+deepsiesta@users.noreply.github.com>
2026-04-10 00:37:02 +0200

    Add numtide cache

--- flake.nix
+++ flake.nix
@@ -9,6 +9,8 @@
       "https://nix-community.cachix.org"
       # CUDA maintainers cache
       "https://cuda-maintainers.cachix.org"
+      # Numtide
+      "https://cache.numtide.com"
       # AAGL
       "https://ezkea.cachix.org"
     ];
@@ -16,6 +18,7 @@
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
     ];
   };
