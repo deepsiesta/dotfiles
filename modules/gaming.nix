c89d076d5e51f254ff59f3b403ab89a268bbc602
Siesta <20047950+deepsiesta@users.noreply.github.com>
2026-04-29 12:38:58 +0200

    Add Faugus Launcher

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -25,6 +25,7 @@
       protonplus
       (lutris.override {extraPkgs = _pkgs: [winetricks];})
       (bottles.override {removeWarningPopup = true;})
+      faugus-launcher
       # Gstreamer
       gst_all_1.gstreamer
       gst_all_1.gst-plugins-base
