e8b68a444f9f8f596da6d7095139441a3cefb179
Siesta <20047950+deepsiesta@users.noreply.github.com>
2026-05-23 17:06:23 +0200

    Add missing overlay

--- modules/development.nix
+++ modules/development.nix
@@ -1,5 +1,11 @@
 {
-  flake.modules.nixos.development = {pkgs, ...}: {
+  flake.modules.nixos.development = {
+    inputs,
+    pkgs,
+    ...
+  }: {
+    nixpkgs.overlays = [inputs.llm-agents.overlays.default];
+
     environment.systemPackages = with pkgs; [
       vscodium-fhs
       llm-agents.opencode
