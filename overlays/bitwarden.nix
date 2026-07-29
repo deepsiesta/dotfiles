946c02748e3f307e4e50573c3416e397b8de9de5
Siesta <20047950+deepsiesta@users.noreply.github.com>
2026-07-29 15:15:53 +0200

    Update, remove bitwarden workaround

--- overlays/bitwarden.nix
+++
@@ -1,9 +0,0 @@
-{
-  flake.modules.nixos.bitwarden-overlay = _: {
-    nixpkgs.overlays = [
-      (final: prev: {
-        bitwarden-desktop = prev.bitwarden-desktop.override {electron_39 = final.electron_39-bin;};
-      })
-    ];
-  };
-}
